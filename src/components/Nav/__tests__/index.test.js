360bf35cb562606fe4f7b8d48ec8a6a5150a4773
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-04-27 00:33:01 -0500

    adding test nav

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -11,5 +11,26 @@ describe('Nav component', () => {
     render(<Nav />);
   });
    
-    // snapshot test
-    })
+  // snapshot test
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+})
+  
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+  })
